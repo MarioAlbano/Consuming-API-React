0dc37376291730c60f46d80e6b7d11437ddabaed
Mario Albano <marioalbanon@gmail.com>
2023-07-13 17:45:23 -0300

    feat: added list tasks in react

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
 import React, { useState } from "react";
+
 const App = () => {
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userAddress, setUserAddress] = useState("");
+  const [userStore, setUserStore] = useState("");
+  const [updatedData, setUpdatedData] = useState([]);
 
-  const [userInfo, setUserInfo] = useState({});
-
-  function sendInfo(e) {
+  const writingArray = (e) => {
     e.preventDefault();
-
-    setUserInfo({
-      name: userName,
-      email: userEmail,
-      address: userAddress,
-    });
-  }
+    setUpdatedData([...updatedData, userStore]);
+    setUserStore("");
+  };
 
   return (
     <div>
-      <h1>Formulário React</h1>
-      <form action="" onSubmit={sendInfo}>
-        <label htmlFor="">Nome: </label>
+      <h1>Lista de tarefas em React</h1>
+
+      <form action="">
         <input
           type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
+          value={userStore}
+          onChange={(e) => setUserStore(e.target.value)}
         />
-        <br />
-        <label htmlFor="">Email: </label>
-        <input
-          type="text"
-          value={userEmail}
-          onChange={(e) => setUserEmail(e.target.value)}
-        />
-        <br />
-        <label htmlFor="">Endereço: </label>
-        <input
-          type="text"
-          value={userAddress}
-          onChange={(e) => setUserAddress(e.target.value)}
-        />
+
+        <button onClick={(e) => writingArray(e)}>Adicionar tarefa</button>
         <br />
         <br />
-        <button type="submit">Enviar formulário</button>
+
+        <h2>Tarefas adicionadas:</h2>
+        <ul>
+          {updatedData.map((task, index) => {
+            return <li key={index}>{task}</li>;
+          })}
+        </ul>
       </form>
-      <br />
-      <br />
-      <br />
-      <h2>Dados registrados:</h2>
-      <h3>Nome: {userInfo.name}</h3>
-      <h3>Nome: {userInfo.email}</h3>
-      <h3>Nome: {userInfo.address}</h3>
     </div>
   );
 };
