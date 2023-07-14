d7ff469e17e96482ce998a6581eac54578ce6880
Mario Albano <marioalbanon@gmail.com>
2023-07-14 17:04:33 -0300

    feat: reseted project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+
+//API: 'https://sujeitoprogramador.com/rn-api/?api=posts'
 
 const App = () => {
-  const [userStore, setUserStore] = useState("");
-  const [updatedData, setUpdatedData] = useState([]);
-
-  useEffect(() => {
-    const savedItems = localStorage.getItem("itemsStore");
-    if (savedItems) {
-      setUpdatedData(JSON.parse(savedItems));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("itemsStore", JSON.stringify(updatedData));
-  }, [updatedData]);
-
-  const writingArray = (e) => {
-    e.preventDefault();
-    setUpdatedData([...updatedData, userStore]);
-    setUserStore("");
-  };
-
-  return (
-    <div>
-      <h1>Lista de tarefas em React</h1>
-
-      <form action="">
-        <input
-          type="text"
-          value={userStore}
-          onChange={(e) => setUserStore(e.target.value)}
-        />
-
-        <button onClick={(e) => writingArray(e)}>Adicionar tarefa</button>
-        <br />
-        <br />
-
-        <h2>Tarefas adicionadas:</h2>
-        <ul>
-          {updatedData.map((task, index) => {
-            return <li key={index}>{task}</li>;
-          })}
-        </ul>
-      </form>
-    </div>
-  );
+  return <div>Teste</div>;
 };
 
 export default App;
