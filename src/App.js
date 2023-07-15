6da443a06e73bc03193cd5c778be44065469cd0c
Mario Albano <marioalbanon@gmail.com>
2023-07-15 12:49:15 -0300

    feat: added header in sinple recipe project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Header from "./components/header/Header";
 //API: 'https://sujeitoprogramador.com/rn-api/?api=posts'
 
 const App = () => {
-  return <div>Teste</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header-main">
+      <h1 className="header-h1">Blog Fitness</h1>
+      <h2 className="header-h2">Dicas para uma vida melhor</h2>
+    </header>
+  );
+};
+
+export default Header;
