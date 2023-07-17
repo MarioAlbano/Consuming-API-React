715fcefeb85478f5ceb09a100e97b4f2a5c2095d
Mario Albano <marioalbanon@gmail.com>
2023-07-17 16:30:10 -0300

    feat: added main part. missing responsiviness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header.js";
+import Main from "./components/Main/Main.js";
 //API: 'https://sujeitoprogramador.com/rn-api/?api=posts'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main/Main.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./Main.css";
+
+const Main = () => {
+  const [myData, setMyData] = useState([]);
+
+  useEffect(() => {
+    const lookingData = async () => {
+      try {
+        const response = await fetch(
+          "https://sujeitoprogramador.com/rn-api/?api=posts"
+        );
+        const data = await response.json();
+        setMyData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    lookingData();
+  }, []);
+
+  return (
+    <main className="main">
+      <section className="main-section-01">
+        <h2 className="main-h2-sec01">
+          Conteúdos relevantes e confiáveis para ajudar você a alcançar uma vida
+          mais saudável.
+        </h2>
+      </section>
+
+      <section className="main-section-02">
+        {myData.map((post) => {
+          return (
+            <div className="main-div-card">
+              <h3 className="card-category">Categoria: {post.categoria}</h3>
+              <h3 className="card-title">{post.titulo}</h3>
+              <img
+                src={post.capa}
+                alt="imagem do post"
+                className="card-image"
+              />
+              <h2 className="card-subtitle">{post.subtitulo}</h2>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default Main;
